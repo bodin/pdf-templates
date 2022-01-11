de15a305480511822d38902145680dc77caf3553
Derek Bodin <derek.bodin@gmail.com>
2022-01-11 17:07:06 -0600

    ci: adding github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,55 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
+    `java-library`
+    `maven-publish`
+    `signing`
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "io.github.bodin"
 
 repositories {
     mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name="sonatype"
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+signing{
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks {
+    wrapper {
+        gradleVersion = "7.3.3"
+    }
+    javadoc {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
 
 dependencies {
@@ -17,18 +59,8 @@ dependencies {
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:1.7.32")
 
-
     // https://mvnrepository.com/artifact/com.github.librepdf/openpdf
     implementation("com.github.librepdf:openpdf:1.3.26")
 
-    implementation("com.github.jknack:handlebars:4.3.0")
-}
-
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
+    compileOnly("com.github.jknack:handlebars:4.3.0")
 }
